f6fde3ef2f88836af0d79e11ce5af87c7e0a7de2
Finlay MacKenzie <fmacke200@caledonian.ac.uk>
2019-12-12 22:54:14 +0000

    Modified configure playbook to use new kubernetes setup script and excluding azure_rm.py in sonarqube analysis

---
+++ setup_scripts/kubernetes.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+sudo apt-get update
+sudo apt-get install apt-transport-https
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install kubectl
+
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | apt-key add -
+
+sudo apt-get update
+sudo apt-get install virtualbox
+
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+chmod +x minikube
+sudo mv minikube /usr/local/bin/
