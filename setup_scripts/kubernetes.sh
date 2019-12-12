954d616b87cb7a1617cdba43b267fa6e69523857
Finlay MacKenzie <fmacke200@caledonian.ac.uk>
2019-12-12 23:18:18 +0000

    Updating kubernetes.sh to automatically say yes when installing packages

--- setup_scripts/kubernetes.sh
+++ setup_scripts/kubernetes.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
 
 sudo apt-get update
-sudo apt-get install apt-transport-https
+sudo apt-get install -y apt-transport-https
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update
-sudo apt-get install kubectl
+sudo apt-get install -y kubectl
 
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | apt-key add -
 
 sudo apt-get update
-sudo apt-get install virtualbox
+sudo apt-get install -y virtualbox
 
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 chmod +x minikube
